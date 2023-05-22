dd265f25b26a99c1f7acba5e3b5381177f97beac
Rodrigo Mantica <manticarodrigo@gmail.com>
2023-05-22 10:38:24 -0600

    Homepage updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,25 @@
+import Image from 'next/image'
 import Link from 'next/link'
+
+const btnClass =
+  'rounded py-2 px-4 w-full text-center text-white font-bold bg-violet-700 hover:bg-violet-900'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 lg:p-24">
-      <h1 className="text-2xl font-bold uppercase">PR Labs</h1>
-      <p className="my-2">
+      <h1 className="sr-only">PRLabs</h1>
+      <Image src="/logo.svg" width={200} height={200} alt="PR Labs Logo" />
+      <p className="mt-2 mb-6 text-center">
         Try our AI-powered tools for Public Relations professionals below:
       </p>
-      <Link
-        href="/brief"
-        className="text-lg font-medium uppercase underline text-center"
-      >
-        Briefing book generator
-      </Link>
-      <Link
-        href="/journalist"
-        className="text-lg font-medium uppercase underline text-center"
-      >
-        Journalist analyzer
-      </Link>
+      <div className="flex flex-col space-y-4 w-64">
+        <Link href="/brief" className={btnClass}>
+          Briefing book generator
+        </Link>
+        <Link href="/journalist" className={btnClass}>
+          Journalist analyzer
+        </Link>
+      </div>
     </main>
   )
 }

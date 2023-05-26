927023c5f52b9412442f28a0ec8be9602f7b4122
Rodrigo Mantica <manticarodrigo@gmail.com>
2023-05-25 21:51:23 -0600

    Change prisma var

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,14 @@
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = global.prisma || new PrismaClient()
+export let prisma
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
-
-export default prisma
+if (typeof window === 'undefined') {
+  if (process.env['NODE_ENV'] === 'production') {
+    prisma = new PrismaClient()
+  } else {
+    if (!global.prisma) {
+      global.prisma = new PrismaClient()
+    }
+    prisma = global.prisma
+  }
+}

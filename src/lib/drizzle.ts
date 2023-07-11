ac8fafe06cbdb4650ce77b11e416b79187970ce4
Your Name <manticarodrigo@gmail.com>
2023-07-11 13:07:39 -0600

    Set ssl config in runtime client.

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -4,7 +4,12 @@ import postgres from 'postgres'
 
 import { article, articleAnalysis, author } from '../../drizzle/schema'
 
-const client = postgres(process.env.POSTGRES_URL_NON_POOLING)
+const client = postgres(process.env.POSTGRES_URL_NON_POOLING, {
+  ssl:
+    process.env.VERCEL_ENV && process.env.VERCEL_ENV !== 'development'
+      ? 'require'
+      : undefined,
+})
 
 export const db = drizzle(client, { logger: true })
 export { article, articleAnalysis, author }

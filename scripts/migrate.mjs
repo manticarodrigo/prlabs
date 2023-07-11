ad48d70af57cc4acf331fdc86e395522ddb7f4cc
Your Name <manticarodrigo@gmail.com>
2023-07-11 13:02:21 -0600

    Check for vercel env

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -2,7 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import postgres from 'postgres'
 
-const sql = postgres(process.env.POSTGRES_URL_NON_POOLING, { max: 1 })
+const sql = postgres(process.env.POSTGRES_URL_NON_POOLING, {
+  max: 1,
+  ssl: process.env.VERCEL_ENV ? 'require' : undefined,
+})
 const db = drizzle(sql)
 
 await migrate(db, { migrationsFolder: 'drizzle' })

661d704e94e80b9223792141b640e5deff0f16e9
Your Name <manticarodrigo@gmail.com>
2023-07-11 09:24:21 -0600

    Bump query limit

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,11 +1,11 @@
 import { clerkClient } from '@clerk/nextjs'
 
 export default async function UsersPage() {
-  const users = await clerkClient.users.getUserList()
+  const users = await clerkClient.users.getUserList({ limit: 100 })
 
   return (
     <main>
-      <h1 className="p-2">Users</h1>
+      <h1 className="p-2">{`Users (${users.length})`}</h1>
       <ul>
         {users.map((user) => (
           <li key={user.id} className="p-2">

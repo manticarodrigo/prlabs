07b23565bcb7215312fc5987aed94b5b307195d3
Your Name <manticarodrigo@gmail.com>
2023-07-11 09:13:13 -0600

    User list page

---
+++ src/app/users/page.tsx
@@ -0,0 +1,21 @@
+import { clerkClient } from '@clerk/nextjs'
+
+export default async function UsersPage() {
+  const users = await clerkClient.users.getUserList()
+
+  return (
+    <main>
+      <h1 className="p-2">Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id} className="p-2">
+            <h2>
+              {user.firstName} {user.lastName}
+            </h2>
+            <p>{user.emailAddresses[0].emailAddress}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}

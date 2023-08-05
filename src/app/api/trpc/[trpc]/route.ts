b89610df5dc99759bbd36608f00c5265d7179b32
Your Name <manticarodrigo@gmail.com>
2023-08-05 09:13:21 -0600

    Allow cors

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -7,6 +7,7 @@ import { appRouter } from "../app.router";
 
 const handler = (request: Request) => {
   console.log(`incoming request ${request.url}`);
+  request.headers.set("Access-Control-Allow-Origin", "*");
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req: request,
